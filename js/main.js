73bbf52f0db1da82e4ff815f8d2956dff601cca0
Joseph Tingsanchali <charnwoot@gmail.com>
2014-08-28 22:59:23 -0500

    only 1 question in View, proceeding to test nextQuestion function

--- js/main.js
+++ js/main.js
@@ -1,16 +1,28 @@
 var Quizzy = (function() {
-	var $quizContainer;
-	var quizData;
-
-	var QuizController = {
-		checkAnswer: function(input, questionModel) {
-			if (input == questionModel.answer) {
-				alert('You got it!!! *cheering*');
-			} else {
-				alert('NOPE!! TRY AGAIN');
-			}
-		}
-	};
+	var $quizContainer,
+			quizData,
+			questionUp = 0,
+			score = 0,
+			questionCount = 0,
+			QuizController = {
+				checkAnswer: function(input, questionModel) {
+					if (input == questionModel.answer) {
+						questionCount++;
+						score++;
+						 $('.submit-button').after($('<p><input type="button" value="Next Question" class="btn btn-lg btn-success">'));
+						alert('Correct! Current score: ' + score + ' out of ' + myFancyQuizData.questions.length);
+						$(".next-question")
+						if (questionCount === myFancyQuizData.questions.length) {
+							alert('Game over. Your score is ' + score + ' out of ' + questionCount + '.');
+						}
+					} else {
+						alert('Wrong. Try again.');
+					}
+				},
+				checkScore: function(){
+					return score;
+				}
+			};
 
 	function QuestionModel(questionData) {
 		this.question = questionData.question;
@@ -18,6 +30,11 @@ var Quizzy = (function() {
 		this.choices  = questionData.choices;
 		this.name     = questionData.name;
 		this.view = new QuestionView(this);
+	}
+
+	function ScoreModel(scoreData) {
+		this.score 			= scoreData.correct;
+		this.totalScore = myFancyQuizData.questions.length;
 	}
 
 	function QuestionView(questionModel) {
@@ -32,7 +49,7 @@ var Quizzy = (function() {
 			name: this.model.name
 		});
 		var $view = $(compiledHtml);
-		$view.find('input[type="submit"]').on('click', function() {
+		$view.find('button[type="submit"]').on('click', function() {
 			QuizController.checkAnswer(
 				$view.find('input[type="radio"]:checked').val(),
 				me.model
@@ -42,16 +59,33 @@ var Quizzy = (function() {
 		$quizContainer.append($view);
 	}
 
+	function ScoreView(scoreModel) {
+		var me = this;
+		this.model = scoreModel;
+		this.template = ('#template-score').html();
+
+		var preppedTemplate = _.template(this.template);
+		var compiledHtml = preppedTemplate({
+			score: this.model.score
+		});
+		var $view = $(compiledHtml);
+
+		$quizContainer.append($view);
+	}
+
 	function startApplication(selector, quizData) {
 		$quizContainer = $(selector);
 
-		$quizContainer.append('<h1>' + quizData.quizTitle + '</h1>');
+		$quizContainer.append('<h1 class="quiz-title">' + quizData.quizTitle + '</h1>');
 
 		var questionModels = [];
-
-		for (var i in quizData.questions) {
-			var model = new QuestionModel(quizData.questions[i]);
-			questionModels.push(model);
+		if (questionUp === 0) {
+			var model = new QuestionModel(quizData.questions[questionUp]);
+			questionModels.push(model);	
+		} else {
+				questionUp += 1;
+				var model = new QuestionModel(quizData.questions[questionUp]);
+				questionModels.push(model);
 		}
 	}
 
---
+++ js/quizData.js
@@ -0,0 +1,118 @@
+var myFancyQuizData = {
+	quizTitle: "rEvErSe JeOpArDy v1.0",
+	questions: [
+		{
+			question: "What decade was J.R.R. Tolkien's \"Lord of the Rings\" triology first published?",
+			answer: "1950s",
+			name: "literature",
+			choices: [
+				"1980s",
+				"1950s",
+				"1910s",
+				"1890s"
+			]
+		},
+		{
+			question: "Which command will remove recursively and forcefully all folders of root?",
+			answer: "rm -rf /",
+			name: "programming",
+			choices: [
+				"rm -rf",
+				"rm -rf \/",
+				"rm",
+				"rm -f \/"
+			]
+		},
+		{
+			question: "What is the official national language of Qatar?",
+			name: "language",
+			answer: "Arabic",
+			choices: [
+				"Arabic",
+				"Farsi",
+				"Urdu",
+				"Pashto"
+			]
+		},
+		{
+			question: "Which actor bought the first Hummer made for civilian use?",
+			name: "language",
+			answer: "Arnold Schwarzenegger",
+			choices: [
+				"Brad Pitt",
+				"Tom Cruise",
+				"Arnold Schwarzenegger",
+				"Shah Rukh Khan"
+			]
+		},
+		{
+			question: "Which Western nation underwent the Quiet Revolution in the 1960s?",
+			name: "history",
+			answer: "Canada",
+			choices: [
+				"USA",
+				"Guatemala",
+				"Puerto Rico",
+				"Canada"
+			]
+		},
+		{
+			question: "What is standard tuning of a violin (low to high)?",
+			name: "music",
+			answer: "G D A E",
+			choices: [
+				"E A D G",
+				"A D G B",
+				"G D A E",
+				"C G D A"
+			]
+		},
+		{
+			question: "Who murdered Robert F. Kennedy in 1968?",
+			name: "history",
+			answer: "Sirhan Sirhan",
+			choices: [
+				"Sirhan Sirhan",
+				"Lee Harvey Oswald",
+				"Nathuram Godse",
+				"James Earl Ray"
+			]
+		},
+		{
+			question: "In golf terminology, what is referred to as an \"eagle\"?",
+			name: "sports",
+			answer: "2 under par",
+			choices: [
+				"1 under par",
+				"2 under par",
+				"par",
+				"+1 over par"
+			]
+		},
+		{
+			question: "Which is the longest river in Australia?",
+			name: "geography",
+			answer: "Murray River",
+			choices: [
+				"Murrumbidgee River",
+				"Lachlan River",
+				"Murray River",
+				"Darling River"
+			]
+		},
+		{
+			question: "What percentage of the cucumber is water?",
+			name: "science",
+			answer: "96%",
+			choices: [
+				"80%",
+				"96%",
+				"60%",
+				"75%"
+			]
+		}
+	]
+};
+
+
+Quizzy.start('#quiz-app', myFancyQuizData);
